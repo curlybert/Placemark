97ea30a9fb76e7d91c2f4bda566f727fe695c0fe
Robert O'Connor <curlybert@gmail.com>
2019-02-05 10:23:38 +0000

    Added simple logging feature

--- app/src/main/java/org/wit/placemark/PlacemarkActivity.kt
+++ app/src/main/java/org/wit/placemark/PlacemarkActivity.kt
@@ -2,11 +2,15 @@ package org.wit.placemark
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.info
 
-class PlacemarkActivity : AppCompatActivity() {
+class PlacemarkActivity : AppCompatActivity(), AnkoLogger {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        info("Placemark Activity started ...")
     }
 }
